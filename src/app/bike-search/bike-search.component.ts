e17f864271e62d14725bdadedda7a711556c99ee
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-04 23:50:10 +0100

    🚧 add bike search & detail

--- src/app/bike-search/bike-search.component.ts
+++ src/app/bike-search/bike-search.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Bike } from '../bike/bike';
 import { FormControl } from '@angular/forms';
+import { mergeMap, debounceTime } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-bike-search',
@@ -16,6 +18,11 @@ export class BikeSearchComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<Bike[]>('/assets/bikes.json').subscribe(bikes => (this.bikes = bikes));
+    this.http.get<Bike[]>(environment.apiBaseUrl + '/bikes').subscribe(bikes => (this.bikes = bikes));
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      mergeMap(query => this.http.get(environment.apiBaseUrl + '/bikes?q=' + query))
+    ).subscribe(console.log);
   }
 }
