4a5f736b410a60419cd8fd74d9172b93d3a4a4c3
Koala <bozonedouard@gmail.com>
2019-12-09 18:25:36 +0100

    Insert memory leak in list view

--- src/app/bike-search/bike-search.component.ts
+++ src/app/bike-search/bike-search.component.ts
@@ -1,33 +1,29 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { startWith, switchMap, exhaustMap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { exhaustMap, startWith, switchMap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 import { Bike } from '../bike/bike';
-import { timer, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bike-search',
   templateUrl: './bike-search.component.html',
   styleUrls: ['./bike-search.component.css']
 })
-export class BikeSearchComponent implements OnInit {
-  bikes: Bike[] = [];
+export class BikeSearchComponent {
   searchControl = new FormControl();
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.searchControl.valueChanges
-      .pipe(
-        startWith(''),
-        switchMap(query =>
-          timer(0, 1000).pipe(exhaustMap(() => this._getBikes(query)))
-        )
+  bikes$ = this.searchControl.valueChanges
+    .pipe(
+      startWith(''),
+      switchMap(query =>
+        timer(0, 1000).pipe(exhaustMap(() => this._getBikes(query)))
       )
-      .subscribe(bikes => (this.bikes = bikes));
-  }
+    );
+
+  constructor(private http: HttpClient) {}
 
   private _getBikes(query: string): Observable<Bike[]> {
     return this.http.get<Bike[]>(environment.apiBaseUrl + '/bikes', {
